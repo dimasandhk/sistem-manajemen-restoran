491f7584d3b3079ab3b9895ff82ffa19d5d6ed45
DaffaEA <daffarajendra191@gmail.com>
2024-06-11 23:19:01 +0700

    apalagi cuy?

--- main.cpp
+++ main.cpp
@@ -165,6 +165,411 @@ class Order : public Customer {
         }
 };
 
+vector<Order> OrderList;
+vector<Menu*>  MenuList;
+vector<People*> PeopleList;
+vector<Employee*> EmployeeList;
+vector<Customer*> CustomerList;
+
+void addemployee() {
+    string name;
+    Date birthDate;
+    int salary, typenum;
+    EmployeeType type;
+
+    cout << "Enter employee's name: ";
+    cin >> name;    
+    cout << "Enter employee's birth date (dd mm yyyy): ";
+    cin >> birthDate.day >> birthDate.month >> birthDate.year;
+    cout << "Enter employee's salary: ";
+    cin >> salary;
+    cout << "Enter employee's type (0 for Chef, 1 for Waiter): ";
+    cin >> typenum;
+    type = (EmployeeType)typenum;
+
+    EmployeeList.push_back(new Employee(name, birthDate, type, salary));    
+}
+
+void ModifyEmployee() {
+    string name, newName;
+    Date birthDate;
+    int salary, typenum;
+    EmployeeType type;
+    
+    for(auto &employee : EmployeeList) {
+        cout << "Enter employee's name: ";
+        cin >> name;
+        if (name == employee->getName()) {
+            cout << "Enter new employee's name: ";
+            cin >> newName;
+            cout << "Enter new employee's birth date (dd mm yyyy): ";
+            cin >> birthDate.day >> birthDate.month >> birthDate.year;
+            cout << "Enter new employee's salary: ";
+            cin >> salary;
+            cout << "Enter new employee's type (0 for Chef, 1 for Waiter): ";
+            cin >> typenum;
+            type = (EmployeeType)typenum;
+
+            employee->setName(newName);
+            employee->setBirthDate(birthDate);
+            employee->setSalary(salary);
+            employee->setType(type);
+        }
+        else {
+            cout << "Employee not found!" << endl;
+        }
+    }
+}
+
+void deleteEmployee() {
+    string name;
+    for(auto &employee : EmployeeList) {
+        cout << "Enter employee's name: ";
+        cin >> name;
+        if (name == employee->getName()) {
+            EmployeeList.erase(remove(EmployeeList.begin(), EmployeeList.end(), employee), EmployeeList.end());
+        }
+        else {
+            cout << "Employee not found!" << endl;
+        }
+    }
+}
+
+void showEmployee() {
+    for(auto &employee : EmployeeList) {
+        cout << employee->intro() << endl;
+    }
+}
+
+void addCostumer() {
+    string name;
+    Date birthDate;
+    int typenum;
+    MemberType type;
+
+    cout << "Enter customer's name: ";
+    cin >> name;    
+    cout << "Enter customer's birth date (dd mm yyyy): ";
+    cin >> birthDate.day >> birthDate.month >> birthDate.year;
+    cout << "Enter customer's type (0 for Basic, 1 for Gold, 2 for Platinum, 3 for Diamond): ";
+    cin >> typenum;
+    type = (MemberType)typenum;
+
+    CustomerList.push_back(new Customer(name, birthDate, type));    
+}
+
+void ModifyCustomer() {
+    string name, newName;
+    Date birthDate;
+    int typenum;
+    MemberType type;
+    
+    for(auto &customer : CustomerList) {
+        cout << "Enter customer's name: ";
+        cin >> name;
+        if (name == customer->getName()) {
+            cout << "Enter new customer's name: ";
+            cin >> newName;
+            cout << "Enter new customer's birth date (dd mm yyyy): ";
+            cin >> birthDate.day >> birthDate.month >> birthDate.year;
+            cout << "Enter new customer's type (0 for Basic, 1 for Gold, 2 for Platinum, 3 for Diamond): ";
+            cin >> typenum;
+            type = (MemberType)typenum;
+
+            customer->setName(newName);
+            customer->setBirthDate(birthDate);
+            customer->setType(type);
+        }
+        else {
+            cout << "Customer not found!" << endl;
+        }
+    }
+}
+
+void deleteCustomer() {
+    string name;
+    for(auto &customer : CustomerList) {
+        cout << "Enter customer's name: ";
+        cin >> name;
+        if (name == customer->getName()) {
+            CustomerList.erase(remove(CustomerList.begin(), CustomerList.end(), customer), CustomerList.end());
+        }
+        else {
+            cout << "Customer not found!" << endl;
+        }
+    }
+}
+
+void showCustomer() {
+    for(auto &customer : CustomerList) {
+        cout << customer->intro() << endl;
+    }
+}
+
+void addMenu() {
+    string name;
+    int price;
+
+    cout << "Enter menu's name: ";
+    cin >> name;
+    cout << "Enter menu's price: ";
+    cin >> price;
+
+    MenuList.push_back(new Menu(name, price));
+}
+
+void ModifyMenu() {
+    string name, newName;
+    int price;
+    
+    for(auto &menu : MenuList) {
+        cout << "Enter menu's name: ";
+        cin >> name;
+        if (name == menu->getName()) {
+            cout << "Enter new menu's name: ";
+            cin >> newName;
+            cout << "Enter new menu's price: ";
+            cin >> price;
+
+            menu->setName(newName);
+            menu->setPrice(price);
+        }
+        else {
+            cout << "Menu not found!" << endl;
+        }
+    }
+}
+
+void deleteMenu() {
+    string name;
+    for(auto &menu : MenuList) {
+        cout << "Enter menu's name: ";
+        cin >> name;
+        if (name == menu->getName()) {
+            MenuList.erase(remove(MenuList.begin(), MenuList.end(), menu), MenuList.end());
+        }
+        else {
+            cout << "Menu not found!" << endl;
+        }
+    }
+}
+
+void showMenu() {
+    for(auto &menu : MenuList) {
+        cout << menu->getName() << " - Rp " << menu->getPrice() << endl;
+    }
+}
+
+void addOrder() {
+    string customerName, menuName;
+    int customerIndex = -1, menuIndex = -1;
+    
+    // Get customer name
+    cout << "Enter customer's name: ";
+    cin >> customerName;
+    
+    // Find customer index
+    for (int i = 0; i < CustomerList.size(); i++) {
+        if (CustomerList[i]->getName() == customerName) {
+            customerIndex = i;
+            break;
+        }
+    }
+    
+    // Check if customer exists
+    if (customerIndex == -1) {
+        cout << "Customer not found!" << endl;
+        return;
+    }
+    
+    // Get menu name
+    cout << "Enter menu's name: ";
+    cin >> menuName;
+    
+    // Find menu index
+    for (int i = 0; i < MenuList.size(); i++) {
+        if (MenuList[i]->getName() == menuName) {
+            menuIndex = i;
+            break;
+        }
+    }
+    
+    // Check if menu exists
+    if (menuIndex == -1) {
+        cout << "Menu not found!" << endl;
+        return;
+    }
+    
+    // Create new order and add menu item
+    OrderList.push_back(Order(CustomerList[customerIndex]->getName(), CustomerList[customerIndex]->getBirthDate(), CustomerList[customerIndex]->getType()));
+    OrderList.back().addItem(*MenuList[menuIndex]);
+}
+
+void modifyOrder() {
+    string customerName, menuName;
+    int customerIndex = -1, menuIndex = -1;
+    
+    // Get customer name
+    cout << "Enter customer's name: ";
+    cin >> customerName;
+    
+    // Find customer index
+    for (int i = 0; i < OrderList.size(); i++) {
+        if (OrderList[i].getName() == customerName) {
+            customerIndex = i;
+            break;
+        }
+    }
+    
+    // Check if customer exists
+    if (customerIndex == -1) {
+        cout << "Customer not found!" << endl;
+        return;
+    }
+    
+    // Get menu name
+    cout << "Enter menu's name: ";
+    cin >> menuName;
+    
+    // Find menu index
+    for (int i = 0; i < MenuList.size(); i++) {
+        if (MenuList[i]->getName() == menuName) {
+            menuIndex = i;
+            break;
+        }
+    }
+    
+    // Check if menu exists
+    if (menuIndex == -1) {
+        cout << "Menu not found!" << endl;
+        return;
+    }
+    
+    // Add menu item
+    OrderList[customerIndex].addItem(*MenuList[menuIndex]);
+}
+
+void deleteOrder() {
+    string customerName;
+    int customerIndex = -1;
+    
+    // Get customer name
+    cout << "Enter customer's name: ";
+    cin >> customerName;
+    
+    // Find customer index
+    for (int i = 0; i < OrderList.size(); i++) {
+        if (OrderList[i].getName() == customerName) {
+            customerIndex = i;
+            break;
+        }
+    }
+    
+    // Check if customer exists
+    if (customerIndex == -1) {
+        cout << "Customer not found!" << endl;
+        return;
+    }
+    
+    // Delete order
+    OrderList.erase(OrderList.begin() + customerIndex);
+}
+
+void showOrder() {
+    for (int i = 0; i < OrderList.size(); i++) {
+        cout << i + 1 << ". " << OrderList[i].intro() << endl;
+    }
+}
+
 int main() {
-    return 0;
+    while(true){
+        int choice;
+        cout << "1.Employee Management\n2.Customer Management\n3.Menu Management\n4.Order Management\n5.Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if (choice == 1) {
+            int choice2;
+            cout << "1.Add Employee\n2.Modify Employee\n3.Delete Employee\n4.Show Employee\n5.Back" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice2;
+            if (choice2 == 1) {
+                addemployee();
+            }
+            else if (choice2 == 2) {
+                ModifyEmployee();
+            }
+            else if (choice2 == 3) {
+                deleteEmployee();
+            }
+            else if (choice2 == 4) {
+                showEmployee();
+            }
+            else if (choice2 == 5) {
+                continue;
+            }
+        }
+        else if (choice == 2) {
+            int choice2;
+            cout << "1.Add Customer\n2.Modify Customer\n3.Delete Customer\n4.Show Customer\n5.Back" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice2;
+            if (choice2 == 1) {
+                addCostumer();
+            }
+            else if (choice2 == 2) {
+                ModifyCustomer();
+            }
+            else if (choice2 == 3) {
+                deleteCustomer();
+            }
+            else if (choice2 == 4) {
+                showCustomer();
+            }
+            else if (choice2 == 5) {
+                continue;
+            }
+        }
+        else if (choice == 3) {
+            int choice2;
+            cout << "1.Add Menu\n2.Modify Menu\n3.Delete Menu\n4.Show Menu\n5.Back" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice2;
+            if (choice2 == 1) {
+                addMenu();
+            }
+            else if (choice2 == 2) {
+                ModifyMenu();
+            }
+            else if (choice2 == 3) {
+                deleteMenu();
+            }
+            else if (choice2 == 4) {
+                showMenu();
+            }
+            else if (choice2 == 5) {
+                continue;
+            }
+        }
+        else if (choice == 4) {
+            int choice2;
+            cout << "1.Add Order\n2.Modify Order\n3.Delete Order\n4.Show Order\n5.Back" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice2;
+            if (choice2 == 1) {
+                addOrder();
+            }
+            else if (choice2 == 2) {
+                modifyOrder();
+            }
+            else if (choice2 == 3) {
+                deleteOrder();
+            }
+            else if (choice2 == 4) {
+                showOrder();
+            }
+            else if (choice2 == 5) {
+                continue;
+            }
+        }
+    }
 }
